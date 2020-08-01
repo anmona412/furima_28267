91ac5f343c446bac5a19edd2fd4966d2d8936d26
anmona412 <anmo.knight412@gmail.com>
2020-08-01 23:15:25 +0900

    入力フォームに全角及全角カナを要求

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name_zen, presence: true
-  validates :given_name_zen, presence: true
-  validates :family_name_kana, presence: true
-  validates :given_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "全角文字で入力してください"} do
+      validates :family_name_zen
+      validates :given_name_zen
+    end
+    
+    with_options format:{with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください"} do
+      validates :family_name_kana 
+      validates :given_name_kana
+    end
+    
+    validates :birthday
+  end
 end
