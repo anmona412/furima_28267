b67befdc09ef0c38afa619af019b5f2783c6256a
anmona412 <anmo.knight412@gmail.com>
2020-08-01 21:34:25 +0900

    ユーザ認証要求、ログイン時のキーのパーミッション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
 before_action :basic_auth
+before_action :authenticate_user!
+before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name_zen,:given_name_zen,:family_name_kana,:given_name_kana,:birthday])
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
