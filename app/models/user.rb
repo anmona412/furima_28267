8addfd6247155781ac0e1b5e5db83b379785ff25
anmona412 <anmo.knight412@gmail.com>
2020-08-02 12:31:12 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    zen_only = /\A[ぁ-んァ-ン一-龥]/.freeze
+    kana_only = /\A[ァ-ヶー－]+\z/.freeze
+    num_and_char = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
     validates :nickname
 
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字で入力してください' } do
+    with_options format: { with: zen_only, message: '全角文字で入力してください' } do
       validates :family_name_zen
       validates :given_name_zen
     end
 
-    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください' } do
+    with_options format: { with: kana_only, message: '全角カタカナで入力してください' } do
       validates :family_name_kana
       validates :given_name_kana
     end
 
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英数半角混合で設定してください' }
+    validates :password, format: { with: num_and_char, message: '英数半角混合で設定してください' }
 
     validates :birthday
   end
