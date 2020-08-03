d3c6816ca6447590f696dbd1543f06532a819c3f
anmona412 <anmo.knight412@gmail.com>
2020-08-03 14:27:24 +0900

    itemsコントローラとルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    
+  end
+end
+
+private
+
+def item_params
+  params.require(:item).premit(
+    :name, :comment, :category_id, :price, :status_id, :burden_id, :prefecture_id, :send_at_id
+  ).merge(user_id: current_user.id)
 end
