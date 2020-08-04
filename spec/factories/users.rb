67c448ec99a57546116b92f2b59a72f74e1fef12
anmona412 <anmo.knight412@gmail.com>
2020-08-04 16:57:43 +0900

    Itemモデルの単体テストコード記載

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname                { Faker::Name.initials(number: 2) }
     email                   { Faker::Internet.free_email }
-    password                { Faker::Internet.password(min_length: 6) }
+    password                { Faker::Internet.password(min_length: 15, mix_case: true) }
     password_confirmation   { password }
     family_name_zen         { Gimei.last.kanji }
     given_name_zen          { Gimei.first.kanji }
