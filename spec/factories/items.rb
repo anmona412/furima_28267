175d84360c1d17418f39bfb03625546bc2eaac26
anmona412 <anmo.knight412@gmail.com>
2020-08-04 15:50:28 +0900

    itemFactory設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name          {Faker::Games::Pokemon.name}
+    comment       {Faker::Lorem.sentence}
+    category_id   {2}
+    price         {Faker::Number.between(from: 333, to: 9999999)}
+    status_id     {2}
+    burden_id     {2}
+    prefecture_id {2}
+    send_at_id    {2}
+    # association   :user
+
+    after(:build) do |item|
+      item.user_id = create(:user).id
+    end
   end
 end
