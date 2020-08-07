aaf210c2ddfa3de31706267744458065583815d3
anmona412 <anmo.knight412@gmail.com>
2020-08-07 14:37:49 +0900

    バリデーション設定

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,12 @@
 class Transaction < ApplicationRecord
+  # アソシエーション
+  belongs_to :item
+  belongs_to_active_hash :prefecture
+
+  # バリデーション
+  with_options presence: true do
+    validtaes :item, :prefecture_id, :city, :address, :building 
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A[0-9]{,11}\z/}
+  end
 end
